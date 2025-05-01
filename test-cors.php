e5a492bd84b446c84152ec4d996ccb9c4ab164c6
Serg Kostyuk <serg.kostyuk@gmail.com>
2025-05-01 14:56:59 -0400

    Continue

--- test-cors.php
+++ test-cors.php
@@ -1,5 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Headers: Content-Type');
 echo json_encode(['ok' => true]);
